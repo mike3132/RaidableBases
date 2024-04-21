2935611ed3dd254057d11347d5340b3cad47cbce
BuildTools <ragnaroksmp32@gmail.com>
2024-04-20 23:12:33 -0400

    Webhook update

--- src/main/java/net/resolutemc/raidablebases/Utils/RaidManager.java
+++ src/main/java/net/resolutemc/raidablebases/Utils/RaidManager.java
@@ -43,7 +43,7 @@ public class RaidManager {
                 raidableBases.getParticleCubeHandler().addCube(player, pos1, pos2);
                 schematicLoader.loadSpecificSchematic(player, string);
 
-                //TODO: Make messages config for this and testing github
+                //TODO: Make messages config for this
                 player.sendMessage("Schematic loaded placeholder");
             }
         }.runTaskLater(RaidableBases.getInstance(), 200L);
